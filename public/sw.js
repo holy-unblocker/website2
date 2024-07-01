a0eb6bddf20ab08f2c932ec7a767f5b8e642d3e7
David Reed <reedswork@proton.me>
2024-07-01 19:34:19 -0400

    fix proxy

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+importScripts("/epoxy/index.js");
+importScripts("/uv/uv.bundle.js");
+importScripts("/uv/uv.config.js");
+importScripts(__uv$config.sw || "/uv/uv.sw.js");
+
+const uv = new UVServiceWorker();
+
+self.addEventListener("fetch", (event) => {
+  // console.log("SW URL:", event.request.url);
+  if (event.request.url.startsWith(location.origin + __uv$config.prefix))
+    event.respondWith(uv.fetch(event));
+});
