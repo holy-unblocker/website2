9b75394e7d2ea34ebc489e52e43c98702755ddbe
David Reed <reedswork@proton.me>
2024-07-23 03:09:52 +0300

    bug fixes

--- src/lib/holyDecrypt.ts
+++ src/lib/holyDecrypt.ts
@@ -5,12 +5,18 @@ let key: string;
 
 const loadKey = () => {
   const e = document.getElementById("eckey")!;
+  if (e === null) return false;
   key = e.innerHTML;
+  e.remove();
+  return true;
 };
 
 loadKey();
 document.addEventListener("astro:page-load", loadKey);
 
 export const holyDecrypt = (text: string) => {
+  if (key === undefined) {
+    if (!loadKey()) throw new Error("key isn't ready yet,. PLEASE WAIT");
+  }
   return CryptoJS.AES.decrypt(text, key).toString(CryptoJS.enc.Utf8);
 };
