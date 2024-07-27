b7dff6be8cb3a761a819ed80fc6df666f126ac5d
David Reed <reedswork@proton.me>
2024-07-27 14:06:10 -0400

    cloak

--- src/lib/cloak.ts
+++ src/lib/cloak.ts
@@ -6,6 +6,77 @@ export interface AppCloak {
   url: string;
 }
 
+export const randomCloaks: (string | AppCloak)[] = [
+  // LMS:
+  // // some  of these are links to the product, not the actual LMS...
+  "https://clever.com/oauth/district-picker",
+  "https://kahoot.it/",
+  // 'https://www.opensesame.com/',
+  "https://www.opensesame.com/user/login",
+  // 'https://www.instructure.com/en-au/canvas',
+  "https://www.olivevle.com/contact-us/",
+  "https://readingsohs.edvance360.com/",
+  // 'https://www.absorblms.com/myabsorb-lms-login-help',
+  "https://suite.vairkko.com/APP/index.cfm/account/Login?reqEvent=main.index&qs=",
+  "https://articulate.com/360",
+  "https://www.blackboard.com/student-resources",
+  // 'https://www.sap.com/products/hcm/hxm-suite.html',
+  // 'https://www.cornerstoneondemand.com/solutions/learning-and-development-lms/',
+  "https://bridgelt.com/",
+  {
+    icon: "https://ssl.gstatic.com/classroom/ic_product_classroom_32.png",
+    title: "Classes",
+    url: "https://classroom.google.com/",
+  },
+  {
+    icon: "https:////ssl.gstatic.com/classroom/ic_product_classroom_32.png",
+    title: "Classes",
+    url: "https://classroom.google.com/",
+  },
+  // ETC:
+  "about:blank",
+  // 'https://openai.com/',
+  "https://getfedora.org/",
+  "https://www.debian.org/",
+  "https://www.opensuse.org",
+  // 'https://nodejs.org/en/',
+  "https://www.microsoft.com/en-us/",
+  "https://nextjs.org/docs/getting-started",
+  "https://addons.mozilla.org/en-US/firefox/",
+  "https://addons.mozilla.org/en-US/firefox/addon/fate-stay-night-trace-on/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=featured",
+  "https://developer.mozilla.org/en-US/plus",
+  "https://huggingface.co/",
+  "https://huggingface.co/docs",
+  "https://www.google.com/chromebook/",
+  "https://workshop.premiumretail.io/external/landing/6b1c3d1225ebd/",
+  "https://https://beta.reactjs.org/",
+  "https://photomath.com/",
+  "https://photomath.com/en/termsofuse",
+  "https://www.mathway.com/Algebra",
+  "https://monkeytype.com/security-policy",
+  "https://www.bbcgoodfood.com/howto/guide/baking-beginners",
+  "https://smallbusiness.withgoogle.com/",
+];
+
+export async function getRandomCloak(): Promise<AppCloak | undefined> {
+  let retries = 5;
+
+  while (true) {
+    let cloak = randomCloaks[~~(Math.random() * randomCloaks.length)];
+    try {
+      if (typeof cloak === "string") cloak = await extractCloakData(cloak);
+      return cloak;
+    } catch (err) {
+      console.error("Error fetching random cloak:", cloak, err);
+      retries--;
+      if (retries === 0) {
+        console.error("Max retries exceeded for cloak");
+        break;
+      }
+    }
+  }
+}
+
 export async function extractCloakData(address: string): Promise<AppCloak> {
   if (address === "about:blank")
     return {
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { db, stripeEnabled } from "@config/apis";
 import { m, isIPBanned, isUserBanned } from "@lib/util";
 import { defineMiddleware } from "astro:middleware";
 import engines from "@lib/searchEngines";
-import { extractCloakData, type AppCloak } from "@lib/cloak";
+import { getRandomCloak, type AppCloak } from "@lib/cloak";
 import { appConfig } from "@config/config";
 import crypto from "node:crypto";
 
@@ -15,77 +15,6 @@ const defaultWispServer =
   typeof appConfig.separateWispServer === "string"
     ? appConfig.separateWispServer
     : "%{ws}//%{host}/api/wisp/";
-
-const randomCloaks: (string | AppCloak)[] = [
-  // LMS:
-  // // some  of these are links to the product, not the actual LMS...
-  "https://clever.com/oauth/district-picker",
-  "https://kahoot.it/",
-  // 'https://www.opensesame.com/',
-  "https://www.opensesame.com/user/login",
-  // 'https://www.instructure.com/en-au/canvas',
-  "https://www.olivevle.com/contact-us/",
-  "https://readingsohs.edvance360.com/",
-  // 'https://www.absorblms.com/myabsorb-lms-login-help',
-  "https://suite.vairkko.com/APP/index.cfm/account/Login?reqEvent=main.index&qs=",
-  "https://articulate.com/360",
-  "https://www.blackboard.com/student-resources",
-  // 'https://www.sap.com/products/hcm/hxm-suite.html',
-  // 'https://www.cornerstoneondemand.com/solutions/learning-and-development-lms/',
-  "https://bridgelt.com/",
-  {
-    icon: "https://ssl.gstatic.com/classroom/ic_product_classroom_32.png",
-    title: "Classes",
-    url: "https://classroom.google.com/",
-  },
-  {
-    icon: "https:////ssl.gstatic.com/classroom/ic_product_classroom_32.png",
-    title: "Classes",
-    url: "https://classroom.google.com/",
-  },
-  // ETC:
-  "about:blank",
-  // 'https://openai.com/',
-  "https://getfedora.org/",
-  "https://www.debian.org/",
-  "https://www.opensuse.org",
-  // 'https://nodejs.org/en/',
-  "https://www.microsoft.com/en-us/",
-  "https://nextjs.org/docs/getting-started",
-  "https://addons.mozilla.org/en-US/firefox/",
-  "https://addons.mozilla.org/en-US/firefox/addon/fate-stay-night-trace-on/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=featured",
-  "https://developer.mozilla.org/en-US/plus",
-  "https://huggingface.co/",
-  "https://huggingface.co/docs",
-  "https://www.google.com/chromebook/",
-  "https://workshop.premiumretail.io/external/landing/6b1c3d1225ebd/",
-  "https://https://beta.reactjs.org/",
-  "https://photomath.com/",
-  "https://photomath.com/en/termsofuse",
-  "https://www.mathway.com/Algebra",
-  "https://monkeytype.com/security-policy",
-  "https://www.bbcgoodfood.com/howto/guide/baking-beginners",
-  "https://smallbusiness.withgoogle.com/",
-];
-
-async function getRandomCloak(): Promise<AppCloak | undefined> {
-  let retries = 5;
-
-  while (true) {
-    let cloak = randomCloaks[~~(Math.random() * randomCloaks.length)];
-    try {
-      if (typeof cloak === "string") cloak = await extractCloakData(cloak);
-      return cloak;
-    } catch (err) {
-      console.error("Error fetching random cloak:", cloak, err);
-      retries--;
-      if (retries === 0) {
-        console.error("Max retries exceeded for cloak");
-        break;
-      }
-    }
-  }
-}
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware(async (context, next) => {
