9604904f0792e602e7e5ab555b4118c7495ec579
David Reed <reedswork@proton.me>
2024-07-27 14:46:08 -0400

    more cloaks

--- src/lib/cloak.ts
+++ src/lib/cloak.ts
@@ -8,39 +8,31 @@ export interface AppCloak {
 
 export const randomCloaks: (string | AppCloak)[] = [
   // LMS:
-  // // some  of these are links to the product, not the actual LMS...
+  // some of these are links to the product, not the actual LMS...
   "https://clever.com/oauth/district-picker",
   "https://kahoot.it/",
-  // 'https://www.opensesame.com/',
   "https://www.opensesame.com/user/login",
-  // 'https://www.instructure.com/en-au/canvas',
   "https://www.olivevle.com/contact-us/",
   "https://readingsohs.edvance360.com/",
-  // 'https://www.absorblms.com/myabsorb-lms-login-help',
   "https://suite.vairkko.com/APP/index.cfm/account/Login?reqEvent=main.index&qs=",
   "https://articulate.com/360",
   "https://www.blackboard.com/student-resources",
-  // 'https://www.sap.com/products/hcm/hxm-suite.html',
-  // 'https://www.cornerstoneondemand.com/solutions/learning-and-development-lms/',
   "https://bridgelt.com/",
   {
     icon: "https://ssl.gstatic.com/classroom/ic_product_classroom_32.png",
     title: "Classes",
     url: "https://classroom.google.com/",
   },
-  {
-    icon: "https:////ssl.gstatic.com/classroom/ic_product_classroom_32.png",
-    title: "Classes",
-    url: "https://classroom.google.com/",
-  },
-  // ETC:
+  // etc:
   "about:blank",
-  // 'https://openai.com/',
+  "https://www.bing.com/",
+  "https://www.google.com/",
+  "https://www.startpage.com/",
   "https://getfedora.org/",
   "https://www.debian.org/",
   "https://www.opensuse.org",
-  // 'https://nodejs.org/en/',
-  "https://www.microsoft.com/en-us/",
+  "https://nodejs.org/",
+  "https://www.microsoft.com/",
   "https://nextjs.org/docs/getting-started",
   "https://addons.mozilla.org/en-US/firefox/",
   "https://addons.mozilla.org/en-US/firefox/addon/fate-stay-night-trace-on/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=featured",
