d220fc557fcade8d388b15fa6159a077d3751113
David Reed <reedswork@proton.me>
2024-07-27 15:26:44 -0400

    store iv in key

--- src/lib/cryptURL.ts
+++ src/lib/cryptURL.ts
@@ -9,11 +9,11 @@ let key: Uint8Array | undefined;
 function getKey(): Uint8Array {
   if (key !== undefined) return key;
 
-  let savedKey = localStorage.getItem("aes_key");
+  let savedKey = localStorage.getItem("aes_key2");
   if (savedKey !== null) return fromBase64(savedKey);
 
-  key = randomBytes(32);
-  localStorage.setItem("aes_key", toBase64(key));
+  key = randomBytes(32 + 16);
+  localStorage.setItem("aes_key2", toBase64(key));
   return key;
 }
 
@@ -21,19 +21,19 @@ const txtenc = new TextEncoder();
 const txtdec = new TextDecoder();
 
 export function encryptURL(part: string) {
-  const iv = randomBytes(16);
-  const stream = cbc(getKey(), iv);
+  const fullKey = getKey();
+  const key = fullKey.slice(0, 32);
+  const iv = fullKey.slice(32);
+  const stream = cbc(key, iv);
   const cipherText = stream.encrypt(txtenc.encode(part));
-  const out = new Uint8Array(16 + cipherText.byteLength);
-  out.set(iv);
-  out.set(cipherText, 16);
-  return toBase64(out);
+  return toBase64(cipherText);
 }
 
 export function decryptURL(part: string) {
   const data = fromBase64(part);
-  const iv = data.slice(0, 16);
-  const cipherText = data.slice(16);
-  const stream = cbc(getKey(), iv);
-  return txtdec.decode(stream.decrypt(cipherText));
+  const fullKey = getKey();
+  const key = fullKey.slice(0, 32);
+  const iv = fullKey.slice(32);
+  const stream = cbc(key, iv);
+  return txtdec.decode(stream.decrypt(data));
 }
