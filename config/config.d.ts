ef1d005277066f113202bac9432660ff189f24ef
David Reed <reedswork@proton.me>
2024-08-08 16:19:41 -0400

    unsteal code

--- config/config.d.ts
+++ config/config.d.ts
@@ -5,31 +5,6 @@
 // DOCUMENTATION AND EXAMPLES OF ALL THIS CRAP
 // CAN BE FOUND IN config.example.js
 
-interface DockerConfig {
-  /**
-   * The type of connection to use to connect to the Docker daemon.
-   * @default "socket"
-   */
-  type?: "http" | "socket";
-  /**
-   * The path to the Docker socket to connect to, if using a socket connection.
-   * @default "/var/run/docker.sock"
-   */
-  socket?: string;
-  /**
-   * The host to connect to, if using an HTTP connection.
-   */
-  host?: string;
-  /**
-   * The port for the docker host, if using an HTTP connection.
-   */
-  port?: number;
-  /**
-   * The Docker network used for connecting to containers
-   */
-  network: string;
-}
-
 export interface AppConfig {
   configName: string;
   hatBadge?: "dev" | "beta" | "normal";
