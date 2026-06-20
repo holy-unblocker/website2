b274558743d102aa83509f9505b2a0310ebf7754
David Reed <reedswork@proton.me>
2026-06-20 13:08:22 -0400

    suppress deprecated warning

--- src/lib/aboutBlank.ts
+++ src/lib/aboutBlank.ts
@@ -3,7 +3,8 @@ export function presentAboutBlank(src: string) {
   if (n === null) throw new Error("Could not create about:blank window");
   const dom = n.document;
   dom.open();
-  dom.write(
+  const write = dom.write.bind(dom) as (markup: string) => void;
+  write(
     `<!doctype html><html><body><iframe style="border:0;position:fixed;top:0;left:0;width:100%;height:100%" id="tool" src="${src}"></iframe><script>window.onfocus=()=>tool.contentWindow.focus()</script></body></html>`,
   );
   dom.close();
